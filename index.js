68ec9f875fa55647407b84a2e7a9f4dce72ed1b4
LNSY <lindsey.asterius@gmail.com>
2024-01-19 10:09:39 -0800

    added markdown styles

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 import { DataroomElement }  from '/dataroom-element.js';
-import "./vendor/markdown-it.min.js";
 import { hljs } from "./vendor/highlight/highlight.min.js";
+import "./vendor/markdown-it.min.js";
 import { wrapHashtags } from './hash-tag.js';
-const md = markdownit();
-
-
-/*
-  
-  Remove Front Matter
-
-  removes the code at the beginning of a markdown file
-  for rendering
-
-*/
-
-function removeFrontMatter(content) {
-  try {
-    const yamlRegex = /^---\n([\s\S]*?)\n---/;
-    return content.replace(yamlRegex, '').trim();
-  } catch(e){
-    return content
+import { removeFrontMatter, parseJSONFrontmatter } from './front-matter.js';
+const md = markdownit({
+  html: true,
+  breaks: true,
+  linkify: true,
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(str, { language: lang }).value;
+      } catch (__) {}
+    }
+    return ''; // use external default escaping
   }
-}
+});
+
 
 class MarkDown extends DataroomElement {
   async initialize(){
     try {
       const data = await this.fetchData();
       const content = removeFrontMatter(data);
+      this.metadata = parseJSONFrontmatter(data);
       this.renderMarkdown(content);
     } catch(e){
       console.error('ERROR', e);
@@ -38,6 +34,7 @@ class MarkDown extends DataroomElement {
   renderMarkdown(content) {
     const renderedContent = md.render(content);
     const hashtags = wrapHashtags(renderedContent);
+
     this.innerHTML = `<div>${hashtags}</div>`;
     
     // Highlight code blocks using hljs
